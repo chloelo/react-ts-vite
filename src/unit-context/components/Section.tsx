67f610aa9b7b411907d91b0784aa4a381560d0c1
chloelo <chloe.lo@ennowell.com>
2023-03-24 10:50:23 +0800

    feat: 🎸 env init

---
+++ src/unit-context/components/Section.tsx
@@ -0,0 +1,11 @@
+interface SectionProps {
+  children: React.ReactNode
+}
+
+export default function Section({ children }: SectionProps) {
+  return (
+    <section className="section">
+      {children}
+    </section>
+  );
+}
